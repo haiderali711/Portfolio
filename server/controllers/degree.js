6c0bd77ea25b0171a4169593b0df5b69b55958b8
haiderali711 <gushaial@student.gu.se>
2020-10-06 23:34:14 +0200

    Update Degree

--- server/controllers/degree.js
+++ server/controllers/degree.js
@@ -5,14 +5,46 @@ const createDegree = (req, res) => {
 
   degree
     .save()
-    .then(result => {
+    .then((result) => {
       res.status(201).json(result);
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({ error: error });
     });
 };
 
+const addCourseID = (req, res) => {
+  var degreeId = req.body.id;
+  var courseId = req.body.courseID;
+  Degree.findOneAndUpdate(
+    { _id: degreeId },
+    { $push: { course: courseId } },
+    { new: true },
+    (err, foundDegree) => {
+      res.status(200).json(foundDegree);
+    }
+  ).catch((error) => {
+    if (error === 404) res.status(404).json();
+    else res.status(500).json({ error: error });
+  });
+};
+
+const deleteDegree = (req, res) => {
+  Course.findOneAndDelete({ _id: req.params.id })
+    .exec()
+    .then((result) => {
+      if (!result) throw 404;
+
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      if (error === 404) res.status(404).json({ error: `degree not found.` });
+      else res.status(500).json({ error: error });
+    });
+};
+
 module.exports = {
-  createDegree
+  createDegree,
+  addCourseID,
+  deleteDegree,
 };
--- server/routes/degree.js
+++ server/routes/degree.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const degreeController = require("../controllers/degree");
 
 router.post("/", degreeController.createDegree);
+router.patch("/:id/courses/:courseID", degreeController.addCourseID);
+router.delete("/:id", degreeController.deleteDegree);
 
 module.exports = router;
