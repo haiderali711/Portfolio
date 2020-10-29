7e3ec28993fefd5961f0b6c443190843ec398fcc
haiali <haiali@student.chalmers.se>
2020-10-29 19:38:48 +0100

    Added css to ProfileItem and do ProfileSkeleton

--- client/src/Components/Profile/ProfileItem.js
+++ client/src/Components/Profile/ProfileItem.js
@@ -1,11 +1,130 @@
-import React from "react";
-import { Card, Tab, Badge, Modal, ListGroup, Col, Row } from "react-bootstrap/";
+import React, { useState } from "react";
+import { Card, Badge, Modal, ListGroup, Col, Row } from "react-bootstrap/";
 import NameUpdateItem from "./NameUpdateItem";
 import ProfileUpdateItem from "./ProfileUpdateItem";
+import "./ProfileItem.css";
 
 export default function Profile({ info, updateInfo }) {
+  const originalObject = {
+    name: false,
+    email: false,
+    username: false,
+    password: false,
+    address: false,
+    city: false,
+    country: false
+  };
+
+  const [currentValues, newValues] = useState(originalObject);
+
+  var updateStatesAccordingly = item => {
+    var temporaryObject = originalObject;
+
+    if (item === 1) {
+      temporaryObject.name = true;
+      newValues(temporaryObject);
+    } else if (item === 2) {
+      temporaryObject.email = true;
+      newValues(temporaryObject);
+    } else if (item === 3) {
+      temporaryObject.username = true;
+      newValues(temporaryObject);
+    } else if (item === 4) {
+      temporaryObject.password = true;
+      newValues(temporaryObject);
+    } else if (item === 5) {
+      temporaryObject.address = true;
+      newValues(temporaryObject);
+    } else if (item === 6) {
+      temporaryObject.city = true;
+      newValues(temporaryObject);
+    } else if (item === 7) {
+      temporaryObject.country = true;
+      newValues(temporaryObject);
+    }
+  };
+
+  var renderUpdateItems = () => {
+    if (currentValues.name === true)
+      return (
+        <NameUpdateItem
+          user_name={info.name}
+          user_surname={info.surname}
+          updateInfo={updateInfo}
+          oldInfo={info}
+        />
+      );
+    else if (currentValues.email === true)
+      return (
+        <ProfileUpdateItem
+          oldInfo={info}
+          itemToUpdate={"email"}
+          inputType={"email"}
+          controlID={"update-email"}
+          formLabel={"Email"}
+          updateInfo={updateInfo}
+        />
+      );
+    else if (currentValues.username === true)
+      return (
+        <ProfileUpdateItem
+          oldInfo={info}
+          itemToUpdate={"username"}
+          inputType={"text"}
+          controlID={"update-username"}
+          formLabel={"Username"}
+          updateInfo={updateInfo}
+        />
+      );
+    else if (currentValues.password === true)
+      return (
+        <ProfileUpdateItem
+          oldInfo={info}
+          itemToUpdate={"password"}
+          inputType={"password"}
+          controlID={"update-password"}
+          formLabel={"Password"}
+          updateInfo={updateInfo}
+        />
+      );
+    else if (currentValues.address === true)
+      return (
+        <ProfileUpdateItem
+          oldInfo={info}
+          itemToUpdate={"address"}
+          inputType={"text"}
+          controlID={"update-address"}
+          formLabel={"Address"}
+          updateInfo={updateInfo}
+        />
+      );
+    else if (currentValues.city === true)
+      return (
+        <ProfileUpdateItem
+          oldInfo={info}
+          itemToUpdate={"city"}
+          inputType={"text"}
+          controlID={"update-city"}
+          formLabel={"City"}
+          updateInfo={updateInfo}
+        />
+      );
+    else if (currentValues.country === true)
+      return (
+        <ProfileUpdateItem
+          oldInfo={info}
+          itemToUpdate={"country"}
+          inputType={"text"}
+          controlID={"update-country"}
+          formLabel={"Country"}
+          updateInfo={updateInfo}
+        />
+      );
+    else return null;
+  };
+
   return (
-    <Tab.Container id="list-group-tabs-example" defaultActiveKey="name">
+    <div>
       <Row>
         <Col>
           <Card className="">
@@ -21,28 +140,73 @@ export default function Profile({ info, updateInfo }) {
               <div>
                 <ListGroup>
                   <ListGroup.Item
-                    className="name-surname-list"
+                    className="profile-list-item"
                     action
                     href="#name"
+                    onClick={() => {
+                      updateStatesAccordingly(1);
+                    }}
                   >
                     {info.name + " " + info.surname}
                   </ListGroup.Item>
-                  <ListGroup.Item action href="#email">
+                  <ListGroup.Item
+                    className="profile-list-item"
+                    action
+                    href="#email"
+                    onClick={() => {
+                      updateStatesAccordingly(2);
+                    }}
+                  >
                     {info.email}
                   </ListGroup.Item>
-                  <ListGroup.Item action href="#username">
+                  <ListGroup.Item
+                    className="profile-list-item"
+                    action
+                    href="#username"
+                    onClick={() => {
+                      updateStatesAccordingly(3);
+                    }}
+                  >
                     {info.username}
                   </ListGroup.Item>
-                  <ListGroup.Item action href="#password">
+                  <ListGroup.Item
+                    className="profile-list-item"
+                    action
+                    href="#password"
+                    onClick={() => {
+                      updateStatesAccordingly(4);
+                    }}
+                  >
                     ***********
                   </ListGroup.Item>
-                  <ListGroup.Item action href="#address">
+                  <ListGroup.Item
+                    className="profile-list-item"
+                    action
+                    href="#address"
+                    onClick={() => {
+                      updateStatesAccordingly(5);
+                    }}
+                  >
                     {info.address}
                   </ListGroup.Item>
-                  <ListGroup.Item action href="#city">
+                  <ListGroup.Item
+                    className="profile-list-item"
+                    action
+                    href="#city"
+                    onClick={() => {
+                      updateStatesAccordingly(6);
+                    }}
+                  >
                     {info.city}
                   </ListGroup.Item>
-                  <ListGroup.Item action href="#country">
+                  <ListGroup.Item
+                    className="profile-list-item"
+                    action
+                    href="#country"
+                    onClick={() => {
+                      updateStatesAccordingly(7);
+                    }}
+                  >
                     {info.country}
                   </ListGroup.Item>
                 </ListGroup>
@@ -50,79 +214,8 @@ export default function Profile({ info, updateInfo }) {
             </Card.Body>
           </Card>
         </Col>
-        <Col>
-          <Tab.Content>
-            <Tab.Pane eventKey="#name">
-              <NameUpdateItem
-                user_name={info.name}
-                user_surname={info.surname}
-                updateInfo={updateInfo}
-                oldInfo={info}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#email">
-              <ProfileUpdateItem
-                oldInfo={info}
-                itemToUpdate={"email"}
-                inputType={"email"}
-                controlID={"update-email"}
-                formLabel={"Email"}
-                updateInfo={updateInfo}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#username">
-              <ProfileUpdateItem
-                oldInfo={info}
-                itemToUpdate={"username"}
-                inputType={"text"}
-                controlID={"update-username"}
-                formLabel={"Username"}
-                updateInfo={updateInfo}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#password">
-              <ProfileUpdateItem
-                oldInfo={info}
-                itemToUpdate={"password"}
-                inputType={"password"}
-                controlID={"update-password"}
-                formLabel={"Password"}
-                updateInfo={updateInfo}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#address">
-              <ProfileUpdateItem
-                oldInfo={info}
-                itemToUpdate={"address"}
-                inputType={"text"}
-                controlID={"update-address"}
-                formLabel={"Address"}
-                updateInfo={updateInfo}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#city">
-              <ProfileUpdateItem
-                oldInfo={info}
-                itemToUpdate={"city"}
-                inputType={"text"}
-                controlID={"update-city"}
-                formLabel={"City"}
-                updateInfo={updateInfo}
-              />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#country">
-              <ProfileUpdateItem
-                oldInfo={info}
-                itemToUpdate={"country"}
-                inputType={"text"}
-                controlID={"update-country"}
-                formLabel={"Country"}
-                updateInfo={updateInfo}
-              />
-            </Tab.Pane>
-          </Tab.Content>
-        </Col>
+        <Col>{renderUpdateItems()}</Col>
       </Row>
-    </Tab.Container>
+    </div>
   );
 }
--- client/src/Views/PortfolioSkeleton.js
+++ client/src/Views/PortfolioSkeleton.js
@@ -1,19 +1,55 @@
 import React, { Component } from "react";
-import { Container, Jumbotron, Row, Col, Button } from "react-bootstrap";
+import { Container, Jumbotron, ListGroup } from "react-bootstrap";
+// import { CSSTransition } from "react-transition-group";
 import AddCourseItem from "../Components/Portfolio/AddCourseItem";
 import "./PortfolioSkeleton.css";
 
 export default class PortfolioSkeleton extends Component {
   constructor(props) {
     super(props);
-    this.state = { itemName: [] };
+    console.log(props);
   }
+
+  renderRelativeItem = page => {
+    if (page === 1) {
+      return <AddCourseItem />;
+    }
+  };
+
   render() {
     return (
       <div>
         <Container>
           <Jumbotron fluid>
-            <Container></Container>
+            <div>
+              <ListGroup className="nav-list-bar" horizontal>
+                <ListGroup.Item className="nav-list-item" action href="#skills">
+                  Skills
+                </ListGroup.Item>
+                <ListGroup.Item
+                  className="nav-list-item"
+                  action
+                  href="#projects"
+                  onClick={() => {}}
+                >
+                  Projects
+                </ListGroup.Item>
+                <ListGroup.Item
+                  className="nav-list-item"
+                  action
+                  href="#courses"
+                >
+                  Courses
+                </ListGroup.Item>
+                <ListGroup.Item
+                  className="nav-list-item"
+                  action
+                  href="#degrees"
+                >
+                  Degrees
+                </ListGroup.Item>
+              </ListGroup>
+            </div>
           </Jumbotron>
         </Container>
       </div>
